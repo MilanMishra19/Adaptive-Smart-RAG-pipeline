9ee1c0958885eb65f32e4e18eac8542f10af0e0c
MilanMishra19 <mishramilan683@gmail.com>
2026-05-25 18:23:45 +0530

    created config.py and basic file structure for modular pipeline

---
+++ rag_pipeline/config.py
@@ -0,0 +1,16 @@
+import os
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+BM25_INDEX_PATH = os.getenv("BM25_INDEX_PATH", "../notebook/bm25_index_10k")
+QDRANT_PATH = os.getenv("QDRANT_PATH", "../notebook/qdrant_database_10k")
+CHUNKS_PATH = os.getenv("CHUNKS_PATH", "../all_chunks_slim.json")
+EMBEDDINGS_PATH = os.getenv("EMBEDDINGS_PATH", "../chunk_embeddings_10k.npy")
+
+BI_ENCODER_MODEL = "all-MiniLM-L6-v2"
+CROSS_ENCODER_MODEL = "cross-encoder/ms-marco-MiniLM-L-12-v2"
+LLM_MODEL = "llama-3.1-8b-instant"
+
+TOP_K_RETRIEVE = 50
+TOP_N_RERANK = 5
+FETCH_K = 100
